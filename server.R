4d904e4a938e572ff189eefb38701ac17cfa276c
matheustonelli <matheusd13@hotmail.com>
2016-08-05 14:34:56 -0300

    Minor fixes

--- server.R
+++ server.R
@@ -1,66 +1,72 @@
 function(input, output, clientData, session) {
-
+  
   #Data = NULL to avoid errors
   v <- reactiveValues(data = NULL)
-
+  
   observe({
     ids <- reactive({
       dbGetQuery(con, paste("select script_run_id from script_run where script_filename = '",
                             input$scriptName,"'", sep=""))
     }) #fim reactive
-
-
+    
+    
     updateSelectInput(session, inputId = "scriptId", choices = ids())
   })#fim observe
-
+  
   #Data
   observeEvent(input$updateButton, {
     v$data <- dbGetQuery(con, paste("select a.app_name, avg(a.duration) as avg_duration,
-                            avg(r.percent_cpu) as avg_percent_cpu,
-                            avg(r.fs_writes) as avg_fs_writes,
-                            avg(r.fs_reads) as avg_fs_reads,
-                            avg(r.max_rss) as avg_max_rss
-                            from app_exec a, script_run s, resource_usage r
-                            where a.script_run_id = s.script_run_id
-                            and r.app_exec_id = a.app_exec_id
-                            and s.script_run_id = '",input$scriptId,"'
-                            group by a.app_name",sep=""))
+                                    avg(r.percent_cpu) as avg_percent_cpu,
+                                    avg(r.fs_writes) as avg_fs_writes,
+                                    avg(r.fs_reads) as avg_fs_reads,
+                                    avg(r.max_rss) as avg_max_rss
+                                    from app_exec a, script_run s, resource_usage r
+                                    where a.script_run_id = s.script_run_id
+                                    and r.app_exec_id = a.app_exec_id
+                                    and s.script_run_id = '",input$scriptId,"'
+                                    group by a.app_name",sep=""))
     #print(v$data)
   })
-
+  
   #Boxes
   output$totalTime <- renderValueBox({
-    if (is.null(v$data)) return()
     time <- dbGetQuery(con, paste("select duration from script_run where script_run_id ='",
                                   input$scriptId,"'", sep=""))
-    valueBox(time, "Total execution time (secs)", icon("area-chart"))
+    if (is.null(v$data))
+      valueBox("0", "Total execution time (secs)", icon("area-chart")) 
+    else
+      valueBox(time, "Total execution time (secs)", icon("area-chart"))
   })
-
+  
   output$totalApps <- renderValueBox({
-    if (is.null(v$data)) return()
     app <- dbGetQuery(con, paste("select count(*) from app_exec where script_run_id ='",
                                  input$scriptId,"' group by script_run_id", sep=""))
-    valueBox(app,"Total apps executed", color = 'yellow',icon("bar-chart"))
+    if (is.null(v$data))
+      valueBox("0","Total apps executed", color = 'yellow',icon("bar-chart"))
+    else
+      valueBox(app,"Total apps executed", color = 'yellow',icon("bar-chart"))
   })
-
+  
   output$finalState <- renderValueBox({
-    if (is.null(v$data)) return()
     final.state <- dbGetQuery(con, paste("select final_state from script_run where script_run_id ='",
                                          input$scriptId,"'", sep=""))
-    valueBox(final.state, "Final state",
-             color = if (identical(as.character(final.state), "SUCCESS")) "green" else "red")
+    if (is.null(v$data))
+      valueBox("None","Final state", color = "red")
+    else
+      valueBox(final.state, "Final state", icon("gears"),
+               color = if (identical(as.character(final.state), "SUCCESS")) "green" else "red")
   })
-
+  
   #Summary
   output$summaryPlot <- renderPrint({
     summary(v$data)
   })
-
+  
   #Table Query
   output$tableQuery <- renderTable({
     print(data.frame(v$data))
   })
-
+  
   #Table Legend
   output$tableLegend <- renderFormattable({
     if (is.null(v$data)) return()
@@ -72,7 +78,7 @@ function(input, output, clientData, session) {
     tableLegend <- data.frame(apps)
     formattable(tableLegend, list(apps = formatter("span", style = x ~ style(color = colorLegend))))
   })
-
+  
   #Plot Duration
   output$plotDuration <- renderPlot({
     if (is.null(v$data)) return()
@@ -80,15 +86,15 @@ function(input, output, clientData, session) {
     barplot(duration, log = "y", las=2, main = "Apps x Duration", beside=TRUE, col=colours,
             ylab = "Avg Duration Time (secs)", cex.names=0.2)
   })
-
+  
   #Plot CPU
   output$plotCpu <- renderPlot({
     if (is.null(v$data)) return()
     cpu <- v$data[ ,3]
     barplot(cpu, log = "y", las=2, main= "Apps x Cpu usage", beside=TRUE, col=colours,
-              ylab = "Avg Cpu usage", cex.names=0.2)
+            ylab = "Avg Cpu usage", cex.names=0.2)
   })
-
+  
   #Plot Bytes Written
   output$plotBytesWritten <- renderPlot({
     if (is.null(v$data)) return()
@@ -96,7 +102,7 @@ function(input, output, clientData, session) {
     barplot(bytes.written, log = "y", las=2, main= "Apps x Written bytes", beside=TRUE, col=colours,
             ylab = "AVG Written Bytes", cex.names=0.2)
   })
-
+  
   #Plot Bytes Read
   output$plotBytesRead <- renderPlot({
     if (is.null(v$data)) return()
@@ -105,81 +111,172 @@ function(input, output, clientData, session) {
     barplot(bytes.read, log = "y", las=2, main= "Apps x Read bytes", beside=TRUE, col=colours,
             ylab = "AVG Read Bytes", cex.names=0.2)
   })
-
+  
   #Plot Memory
   output$plotMemory <- renderPlot({
     if (is.null(v$data)) return()
     memoria <- v$data[ ,6]
     barplot(memoria,log = "y", las=2, main= "Apps x Memory",beside=TRUE,col=colours,
             ylab = "AVG Used Memory",cex.names=0.5)
-    })
-
+  })
+  
   #Plot Regression
   output$plotRegression <- renderPlot({
-    script_name <- dbGetQuery(con, paste("select script_filename from script_run where script_run_id='",
-                                             input$scriptId,"'", sep = ""))
-
     #Query Regression
-    input = dbGetQuery(con, paste("select s.script_run_id, f.size from staged_in i, file f, app_exec a, script_run s
-                                   where f.file_id = i.file_id
-                                   and a.app_exec_id = i.app_exec_id
-                                   and a.script_run_id = s.script_run_id
-                                   and s.script_filename = '",script_name,"'
-                                   and i.file_id not in (select o.file_id from staged_out o)", sep = ""))
-    duration = dbGetQuery(con, paste("select script_run_id, duration from script_run where script_filename='",script_name, "'", sep = ""))
-
-    input$size <- as.numeric(input$size)
-    sum.size <- aggregate(input$size ~ input$script_run_id, input, FUN = function(x){sum(as.numeric(x))})
-
+    data = dbGetQuery(con, paste("select s.script_run_id, f.size from staged_in i, file f, app_exec a, script_run s
+                                 where f.file_id = i.file_id
+                                 and a.app_exec_id = i.app_exec_id
+                                 and a.script_run_id = s.script_run_id
+                                 and s.script_filename = '",input$scriptName,"'
+                                 and i.file_id not in (select o.file_id from staged_out o)", sep = ""))
+    duration = dbGetQuery(con, paste("select script_run_id, duration from script_run where script_filename='",input$scriptName, "'", sep = ""))
+    data$size <- as.numeric(data$size)
+    sum.size <- aggregate(data$size ~ data$script_run_id, data, FUN = function(x){sum(as.numeric(x))})
+    
     #Join
     colnames(sum.size) <- c("script_run_id", "size")
-    result <- join(sum.size, duration)
-
-    #Plot
-    plot(result$size, result$duration, xlab="Input size", ylab="Total execution time (s)", pch=20,
-         xlim=range(result$size,result$size))
-
+    data <- join(sum.size, duration)
+    
+    indexes = sort(sample(nrow(data), nrow(data)*.7))
+    
+    #Training
+    train = data[indexes,]
+    plot.train = plot(train$size, train$duration, xlab="Input size", ylab="Total execution time (s)", pch = 20, 
+                      xlim=range(train$size, train$size))
+    #Test
+    test = data[-indexes,]
+    points(test$size, test$duration, pch = 20, col = "red")
+    
     #Linear
-    model <- lm(result$duration ~ result$size) # (y~x)
-    abline(model, col = colours)
+    model <- lm(train$duration ~ train$size) # (y~x)
+    abline(model, col=colours[1])
     #Quadratic
-    model.2 <- lm(result$duration ~ poly(result$size, 2, raw=TRUE), result)
+    model.2 <- lm(train$duration ~ poly(train$size, 2, raw=TRUE), train)
     curve(coefficients(model.2)[1] + coefficients(model.2)[2]*x + coefficients(model.2)[3]*x*x, add = T, col = colours[2])
     #Description
     model.legend = c("Linear", "Quadratic")
     legend("topleft", xpd = TRUE, model.legend, col=colours, lty = 1, cex = 1, bty="n")
+    
+    #Table Description
+    output$tableDescription <- renderFormattable({
+      sets <- c("Training", "Testing")
+      points <- c(nrow(train), nrow(test))
+      tableDescription <- data.frame(sets, points)
+      formattable(tableDescription, list(sets = formatter("span", style = x ~ style(color = c("black", "red")))))
+    })
+    
+    output$textDataSumamry <- renderTable({
+      sum.table <- rbind(t(summary(data$size)), t(summary(data$duration)))
+      rownames(sum.table) <- c("size", "duration")
+      print(sum.table)
+    })
+    
   })
+  
 
+#  downloadFormattInput <- reactive({
+#    switch(input$downloadFormatt,
+#           "Duration" = 1,
+#           "Cpu Usage"=2,
+#           "Written Bytes"=3,
+#           "Read Bytes"=4,
+#           "Memory used"=5,
+#           "All Graphis"=6)
+#  })
+  
   #Download plot
   output$downloadButton <- downloadHandler(
     filename = function(){
-      paste(input$scriptId,input$var3,sep=".")
+      paste(input$scriptId,input$downloadFormatt,sep=".")
     },
     content = function(file){
-      if(input$var3 == "png")
-        png(file)
-      else
-        pdf(file)
-
-      {
       #Duration
-      barplot(duration, log = "y", las=2, main = "Tarefa x Duração", beside=TRUE, col=colours,
-              ylab = "Tempo médio de duração", xlab = "Tarefa", cex.names=0.2)
-              legend("topleft", xpd = TRUE, legend = apps, col=colours, lty = 1, cex = 1, bty="n")
-      #CPU
-      barplot(cpu, log = "y", las=2, main= "Tarefa x Cpu", beside=TRUE, col=colours,
-              ylab = "Avg CPU",xlab = "Tarefa", cex.names=0.2)
-      #Bytes
-      barplot(bytes, log = "y", las=2, main= "Tarefa x Bytes escritos", beside=TRUE, col=colours,
-             ylab = "Bytes escritos",xlab = "Tarefa", cex.names=0.2)
-
-      #Memory
-      barplot(memoria,log = "y", las=2, main= "Tarefa x Memoria",beside=TRUE,col=colours,
-              ylab = "Tempo de duracao",xlab = "Tarefa",cex.names=0.5)
+      if(input$downloadFormatt == "1"){
+        png(file)
+      {
+        apps <- v$data[ ,1]
+        duration <- v$data[ ,2]
+        barplot(duration, log = "y", las=2, main = "Apps x Duration", beside=TRUE, col=colours,
+                ylab = "Avg Duration Time (secs)", cex.names=0.2)
+        legend("topleft", xpd = TRUE, legend = apps, col=colours, lty = 1, cex = 1, bty="n")
       }
-
-      dbDisconnect(con)
-      dev.off()
+      } else
+      #Cpu Usage
+      if(input$downloadFormatt == "2"){
+        png(file)
+      {  
+        apps <- v$data[ ,1]
+        cpu <- v$data[ ,3]
+        barplot(cpu, log = "y", las=2, main= "Apps x Cpu usage", beside=TRUE, col=colours,
+                ylab = "Avg Cpu usage", cex.names=0.2)
+        legend("topleft", xpd = TRUE, legend = apps, col=colours, lty = 1, cex = 1, bty="n")
+       }
+      } else
+      #Written Bytes
+      if(input$downloadFormatt == "3"){
+        png(file)
+      {  
+        apps <- v$data[ ,1]
+        bytes.written <- v$data[ ,4]
+        barplot(bytes.written, log = "y", las=2, main= "Apps x Written bytes", beside=TRUE, col=colours,
+                ylab = "AVG Written Bytes", cex.names=0.2)
+        legend("topleft", xpd = TRUE, legend = apps, col=colours, lty = 1, cex = 1, bty="n")
       }
-    )
+      } else
+      #Read Bytes
+      if(input$downloadFormatt == "4"){
+        png(file)
+      {  
+        apps <- v$data[ ,1]
+        bytes.read <- v$data[ ,5]
+        bytes.read <- bytes.read[bytes.read != "0"]
+        barplot(bytes.read, log = "y", las=2, main= "Apps x Read bytes", beside=TRUE, col=colours,
+                ylab = "AVG Read Bytes", cex.names=0.2)
+        legend("topleft", xpd = TRUE, legend = apps, col=colours, lty = 1, cex = 1, bty="n")
+      }
+      } else
+      #Memory used
+      if(input$downloadFormatt == "5"){
+        png(file)
+      {  
+        apps <- v$data[ ,1]
+        memoria <- v$data[ ,6]
+        barplot(memoria,log = "y", las=2, main= "Apps x Memory",beside=TRUE,col=colours,
+                ylab = "AVG Used Memory",cex.names=0.5)
+        legend("topleft", xpd = TRUE, legend = apps, col=colours, lty = 1, cex = 1, bty="n")
+      }
+      } else
+      #All Graphis
+      if(input$downloadFormatt == "6"){
+      pdf(file)
+      {
+        apps <- v$data[ ,1]
+        bytes <- v$data[ ,4]
+        duration <- v$data[ ,2]
+        cpu <- v$data[ ,3]
+        memoria <- v$data[ ,6]
+        #Duration
+        barplot(duration, log = "y", las=2, main = "Tarefa x Duração", beside=TRUE, col=colours,
+                ylab = "Tempo médio de duração", xlab = "Tarefa", cex.names=0.2)
+        legend("topleft", xpd = TRUE, legend = apps, col=colours, lty = 1, cex = 1, bty="n")
+        #CPU
+        barplot(cpu, log = "y", las=2, main= "Tarefa x Cpu", beside=TRUE, col=colours,
+                ylab = "Avg CPU",xlab = "Tarefa", cex.names=0.2)
+        #Bytes
+        barplot(bytes, log = "y", las=2, main= "Tarefa x Bytes escritos", beside=TRUE, col=colours,
+                ylab = "Bytes escritos",xlab = "Tarefa", cex.names=0.2)
+        
+        #Memory
+        barplot(memoria,log = "y", las=2, main= "Tarefa x Memoria",beside=TRUE,col=colours,
+                ylab = "Tempo de duracao",xlab = "Tarefa",cex.names=0.5)
+      }
+      }
+          graphics.off()
+    }
+  )
+  
 }
+
+
+  
+
--- ui.R
+++ ui.R
@@ -1,81 +1,97 @@
 dashboardPage(skin = "blue",
-      dashboardHeader(title = "HPSW-Prof", titleWidth = 300),
-
-      dashboardSidebar(width = 300,
-        sidebarMenu(id = "sidebarmenu",
-          selectInput("scriptName", "Script Name", choices = script_names),
-          menuItem("Profiler", tabName = "profiler"),
-          conditionalPanel("input.sidebarmenu === 'profiler'",
-                           selectInput("scriptId", "Script Id", choices = NULL),
-                           actionButton("updateButton", "Update",
-                                        tags$style(type='text/css', "#updateButton { width: 100px;
-                                        margin:auto; display:block; }"))
-          ),
-
-          menuItem("Predictions", tabName = "predictions"),
-
-          menuItem("Download",
-                   radioButtons(inputId = "downloadFormatt", label = "Choose a format: ", choices = list("png","pdf"), inline = TRUE),
-                   downloadButton(outputId = "downloadButton", label = "Download",
-                                  tags$style(type='text/css', "#downloadButton { width: 100px; margin:auto; display:block; }"))
-          ),
-          menuItem("Source code", icon = icon("file-code-o"),
-                    href = "https://github.com/mmondelli/swift-prof"
-          )
-        )#fim menu
-      ),
-      dashboardBody(
-        tabItems(
-          tabItem(tabName = "profiler",
-            fluidRow(
-              valueBoxOutput("totalTime"),
-              valueBoxOutput("totalApps"),
-              valueBoxOutput("finalState")
-            ),
-            fluidRow(
-              box(
-                title = "Legend", status = "primary", solidHeader = TRUE,
-                collapsible = TRUE, formattableOutput("tableLegend"), width = 3
+              dashboardHeader(title = "HPSW-Prof", titleWidth = 300),
+              
+              dashboardSidebar(width = 300,
+                               sidebarMenu(id = "sidebarmenu",
+                                           selectInput("scriptName", "Script Name", choices = script_names),
+                                           menuItem("Profiler", tabName = "profiler"),
+                                           conditionalPanel("input.sidebarmenu === 'profiler'",
+                                                            selectInput("scriptId", "Script Id", choices = NULL),
+                                                            actionButton("updateButton", "Update",
+                                                                         tags$style(type='text/css', "#updateButton { width: 100px;
+                                                                                    margin:auto; display:block; }"))
+                                                                         ),
+                                           
+                                           menuItem("Predictions", tabName = "predictions"),
+                                           
+                                           
+                                           menuItem("Download",
+                                                    radioButtons(inputId = "downloadFormatt", label = "Choose a type: ",
+                                                                choices = list("Duration"=1,"Cpu Usage"=2,"Written Bytes"=3,
+                                                                               "Read Bytes"=4,"Memory used"=5,"All Graphis"=6),selected = 6),
+                                                    downloadButton(outputId = "downloadButton", label = "Download")
+                                                    
+                                           ),
+                                           menuItem("Source code", icon = icon("file-code-o"),
+                                                    href = "https://github.com/mmondelli/swift-prof"
+                                           )
+                                           )#fim menu
               ),
-
-              tabBox(
-                title = "Summary",
-                id = "tabsetSummary", height = "250px", width = 9,
-                tabPanel("Summary", verbatimTextOutput("summaryPlot")),
-                tabPanel("Table", tableOutput("tableQuery"))
-              )#fim tabbox
-            ), #fim fluidRow
-
-          fluidRow(
-            box(
-              title = "Duration", background = "black", solidHeader = TRUE, collapsible = TRUE,
-              plotOutput(outputId = "plotDuration")
-            ),
-
-            box(
-              title = "Cpu Usage", background = "black", solidHeader = TRUE, collapsible = TRUE,
-              plotOutput(outputId = "plotCpu")
-            ),
-
-            box(
-              title = "Written Bytes", background = "black", solidHeader = TRUE, collapsible = TRUE,
-              plotOutput(outputId = "plotBytesWritten")
-            ),
-
-            box(
-              title = "Read Bytes", background = "black", solidHeader = TRUE, collapsible = TRUE,
-              plotOutput(outputId = "plotBytesRead")
-            ),
-
-            box(
-              title = "Memory used", background = "black", solidHeader = TRUE, collapsible = TRUE,
-              plotOutput(outputId = "plotMemory")
-            )
-          )#fim fluidRow
-         ),#fim tabItem
-         tabItem("prediction"
-         )
-        )#fim tabItems
-      )#fim body
-    )
+              dashboardBody(
+                tabItems(
+                  tabItem(tabName = "profiler",
+                          fluidRow(
+                            valueBoxOutput("totalTime"),
+                            valueBoxOutput("totalApps"),
+                            valueBoxOutput("finalState")
+                          ),
+                          fluidRow(
+                            box(
+                              title = "Legend", status = "primary", solidHeader = TRUE,
+                              collapsible = TRUE, formattableOutput("tableLegend"), width = 3
+                            ),
+                            
+                            tabBox(
+                              title = "Summary",
+                              id = "tabsetSummary", height = "250px", width = 9,
+                              tabPanel("Table", tableOutput("tableQuery")),
+                              tabPanel("Summary", verbatimTextOutput("summaryPlot"))
+                            )#fim tabbox
+                          ), #fim fluidRow
+                          
+                          fluidRow(
+                            box(
+                              title = "Duration", background = "black", solidHeader = TRUE, collapsible = TRUE,
+                              plotOutput("plotDuration")
+                            ),
+                            
+                            box(
+                              title = "Cpu Usage", background = "black", solidHeader = TRUE, collapsible = TRUE,
+                              plotOutput("plotCpu")
+                            ),
+                            
+                            box(
+                              title = "Written Bytes", background = "black", solidHeader = TRUE, collapsible = TRUE,
+                              plotOutput("plotBytesWritten")
+                            ),
+                            
+                            box(
+                              title = "Read Bytes", background = "black", solidHeader = TRUE, collapsible = TRUE,
+                              plotOutput("plotBytesRead")
+                            ),
+                            
+                            box(
+                              title = "Memory used", background = "black", solidHeader = TRUE, collapsible = TRUE,
+                              plotOutput("plotMemory")
+                            )
+                          )#fim fluidRow
+                  ),#fim tabItem
+                  tabItem(tabName = "predictions",
+                          fluidRow(
+                            box(
+                              title = "Regression ", solidHeader = TRUE,
+                              collapsible = TRUE,
+                              plotOutput("plotRegression", height = 500)
+                            ),
+                            box(title = "Data Summary", width = 6, status = "warning",
+                                tableOutput("textDataSumamry")
+                            ),
+                            box(title = "Regression Summary", width = 6, status = "warning",
+                                formattableOutput("tableDescription")
+                            )
+                          )
+                  )#fim tabItem
+                )#fim tabItems
+              )#fim body
+)
 
