9e4f45038815d7bb05f44b1ae95ca62dd425f204
Maria Luiza Mondelli <malumondelli@gmail.com>
2017-02-13 16:01:30 -0200

    Update global.R

--- global.R
+++ global.R
@@ -14,7 +14,7 @@ colours <- brewer.pal(12,"Paired")
 ajuste <- par(mar=c(1,4,3,1)+0.6)
 
 drv <- dbDriver("PostgreSQL")
-con <- dbConnect(SQLite(), "~/Dropbox/Artigos/2015-OswaldoTrelles (1)/database/swift_provenance041.db")
+con <- dbConnect(SQLite(), "~/swift_provenance.db")
 #con <- dbConnect(drv, dbname = "swift_provenance",
 #                 host = "localhost", port = 5432,user = "postgres", password = 'postgres')
 
